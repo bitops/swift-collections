5a8f86c6c07848223aa2ccd3380b5e7dbd6afc19
Sebastian Wittenkamp <sebastian@bitops.io>
2014-07-16 21:02:05 -0700

    Use prettier syntax

--- SwiftCollections.swift
+++ SwiftCollections.swift
@@ -1,72 +1,68 @@
 extension Array {
     func each(iterator: (T) -> Void) -> Array {
-        for item in self {
-            iterator(item)
-        }
+        for item in self { iterator(item) }
         return self
     }
-
+    
     func partition(iterator: (T) -> Bool) -> (Array<T>, Array<T>) {
         var ifTrue = Array<T>()
         var ifFalse = Array<T>()
-        self.each {
-            if iterator($0) == true {
-                ifTrue.append($0)
+        self.each { (item: T) -> Void in
+            if iterator(item) == true {
+                ifTrue.append(item)
             } else {
-                ifFalse.append($0)
+                ifFalse.append(item)
             }
         }
         return (ifTrue, ifFalse)
     }
-
+    
     func all(iterator: (T) -> Bool) -> Bool {
         var result = true
-        self.each {
-            if iterator($0) == false {
+        self.each { (item: T) -> Void in
+            if iterator(item) == false {
                 result = false; return
             }
         }
         return result
     }
-
+    
     func any(iterator: (T) -> Bool) -> Bool {
         var result = false
-        self.each {
-            if iterator($0) == true {
+        self.each { (item: T) -> Void in
+            if iterator(item) == true {
                 result = true; return
             }
         }
         return result
     }
-
+    
     func none(iterator: (T) -> Bool) -> Bool {
         var result = true
-        self.each {
-            if iterator($0) == true {
+        self.each { (item: T) -> Void in
+            if iterator(item) == true {
                 result = false; return
             }
         }
         return result
     }
-
+    
     func first() -> T? {
         if self.count >= 1 {
             return self[0]
         } else {
             return nil
         }
-        
     }
-
+    
     func last() -> T? {
         if self.count >= 1 {
             return self[self.count - 1]
         } else {
             return nil
         }
-        
     }
-
+    
     func take(count: Int) -> Array {
         var result = Array<T>()
         for idx in 1...count {
@@ -74,7 +70,7 @@ extension Array {
         }
         return result
     }
-
+    
     func drop(count: Int) -> Array {
         var result = Array<T>()
         var end = self.count - 1
@@ -83,15 +79,14 @@ extension Array {
         }
         return result
     }
-
+    
     func find(iterator: (T) -> Bool) -> T? {
         var result : T?
-        self.each {
-            if iterator($0) == true {
-                result = $0; return
+        self.each { (item: T) -> Void in
+            if iterator(item) == true {
+                result = item; return
             }
         }
         return result
     }
 }
-
